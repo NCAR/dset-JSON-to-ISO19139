9f671536374cfa54f1a356a4d73464b368402649
bonnland <bonnland@ucar.edu>
2021-01-29 15:07:49 -0700

    DSET-504  Add extra DOIs for translation

--- bin/run_doi_batch.sh
+++ bin/run_doi_batch.sh
@@ -1,7 +1,11 @@
 
-DOI_SUFFIXES="4xv0-fg55 t353-c093 9n3z-7x72 7we1-8k84 z9nq-8g12 b92r-gt40 d6ws8r93 9zx1-jq74 \
-              60hz-ry38 sv4e-7z49 1TDC-0Z47 fv7s-ax27 82ny-4074 p8es-mc74 k9vg-t494 789w-m137 \
-              1k0w-2272 chk8-fx07 rgpy-g566 8r12-hs65 7m8g-ja33 qan9-we09"
+# January 2021
+#DOI_SUFFIXES="4xv0-fg55 t353-c093 9n3z-7x72 7we1-8k84 z9nq-8g12 b92r-gt40 d6ws8r93 9zx1-jq74 \
+#              60hz-ry38 sv4e-7z49 1TDC-0Z47 fv7s-ax27 82ny-4074 p8es-mc74 k9vg-t494 789w-m137 \
+#              1k0w-2272 chk8-fx07 rgpy-g566 8r12-hs65 7m8g-ja33 qan9-we09"
+
+# February 2021
+DOI_SUFFIXES="4xv0-fg55 0dxg-nn57 1a8d-yh72"
 
 echo "DOI_SUFFIXES= " $DOI_SUFFIXES
 
